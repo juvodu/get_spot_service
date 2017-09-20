57a44d76511f316c2455212c35d2e3fdad9ded4b
juvodu <juri.ticho@gmail.com>
2017-09-20 22:16:23 +0200

    Weatherservice
    - add weather service and test
    - increase api gateway max requests

---
+++ src/main/java/com/juvodu/service/WeatherService.java
@@ -0,0 +1,33 @@
+package com.juvodu.service;
+
+import com.juvodu.database.model.Position;
+import com.juvodu.forecast.controller.WWOMClient;
+import com.juvodu.forecast.exception.WWOMClientException;
+import com.juvodu.forecast.model.Forecast;
+
+/**
+ * Service for retrieval of the maritim weather conditions.
+ *
+ * @author Juvodu
+ */
+public class WeatherService {
+
+
+    private WWOMClient client;
+
+    public WeatherService(){
+        this.client = new WWOMClient();
+    }
+
+    /**
+     * Get the maritim weather forecast for a position
+     *
+     * @param position
+     * @return
+     * @throws WWOMClientException
+     */
+    public Forecast getForecastForPosition(Position position) throws WWOMClientException {
+
+        return client.getForecast(String.format("%s, %s", position.getLatitude(), position.getLongitude()));
+    }
+}
---
+++ src/test/java/com/juvodu/service/WeatherServiceTest.java
@@ -0,0 +1,35 @@
+package com.juvodu.service;
+
+import com.juvodu.database.model.Position;
+import com.juvodu.forecast.model.Forecast;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+
+/**
+ * Test suite for the WeatherService
+ *
+ * @author Juvodu
+ */
+public class WeatherServiceTest {
+
+    private static WeatherService weatherService;
+    private final Position hossegor = new Position(43.671223, -1.441445);
+
+    @BeforeClass
+    public static void beforeClass() {
+
+        weatherService = new WeatherService();
+    }
+
+    @Test
+    public void givenPositionWhengetForcastForPositionThenReturnForecast() throws Exception {
+
+        Forecast forecast = weatherService.getForecastForPosition(hossegor);
+        assertNotNull(forecast);
+
+    }
+
+}
