1f34ba550820a44cd9da2e086054b2d2f78d7c05
juvodu <juri.ticho@gmail.com>
2017-11-26 17:56:34 +0100

    Get Favourites Lambda function

--- src/main/java/com/juvodu/serverless/handler/GetFavoritesHandler.java
+++ src/main/java/com/juvodu/serverless/handler/GetFavoritesHandler.java
@@ -2,9 +2,12 @@ package com.juvodu.serverless.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.juvodu.database.model.Favorite;
 import com.juvodu.serverless.ParameterParser;
 import com.juvodu.serverless.response.ApiGatewayResponse;
 import com.juvodu.serverless.response.CrudResponse;
+import com.juvodu.service.FavoriteService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.util.Map;
@@ -25,15 +28,22 @@ public class GetFavoritesHandler implements RequestHandler<Map<String, Object>, 
         LOG.info("Query String parameters: " + queryStringParameters);
         int statusCode = 200;
 
+        FavoriteService<Favorite> favoriteService = new FavoriteService<>(Favorite.class);
         Object body = null;
 
         try {
 
             Map<String, String> parameters = ParameterParser.getParameters(queryStringParameters);
             String userId = parameters.get("userId");
+            String limitStr = parameters.get("limit");
 
-            //TODO: retrieve favorites
+            // default list size is 10
+            int limit = 10;
+            if(StringUtils.isNotBlank(limitStr)){
+                limit = Integer.parseInt(limitStr);
+            }
 
+            body = favoriteService.getFavoritesByUser(userId, limit);
 
         } catch (Exception e) {
 
