f599ccddfa6aa74c75f05deec9592aeb5e0771c3
juvodu <juri.ticho@gmail.com>
2017-11-29 19:38:24 +0100

    GetUserHandler

---
+++ src/main/java/com/juvodu/serverless/handler/GetUserHandler.java
@@ -0,0 +1,52 @@
+package com.juvodu.serverless.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.juvodu.database.model.User;
+import com.juvodu.serverless.ParameterParser;
+import com.juvodu.serverless.response.ApiGatewayResponse;
+import com.juvodu.serverless.response.CrudResponse;
+import com.juvodu.service.UserService;
+import org.apache.log4j.Logger;
+
+import java.util.Map;
+
+/**
+ * Handler which retrieves a user
+ *
+ * @author Juvodu
+ */
+public class GetUserHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private static final Logger LOG = Logger.getLogger(GetUserHandler.class);
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+        String queryStringParameters = input.get("queryStringParameters").toString();
+        LOG.info("Query String parameters: " + queryStringParameters);
+        int statusCode = 200;
+
+        UserService<User> userService = new UserService(User.class);
+        Object body;
+
+        try {
+
+            Map<String, String> parameters = ParameterParser.getParameters(queryStringParameters);
+            String id = parameters.get("id");
+
+            body = userService.getByHashKey(id);
+
+        } catch (Exception e) {
+
+            statusCode = 500;
+            body = new CrudResponse("Error during get user by id:" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return ApiGatewayResponse.builder()
+                .setStatusCode(statusCode)
+                .setObjectBody(body)
+                .build();
+    }
+}
