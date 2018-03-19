b3104d6789cdf0790d828f57a746ec64e82e833d
juvodu <juri.ticho@gmail.com>
2018-03-19 22:16:13 +0100

    Update Platform Application Endpoint

--- src/main/java/com/juvodu/util/Constants.java
+++ src/main/java/com/juvodu/util/Constants.java
@@ -8,7 +8,7 @@ package com.juvodu.util;
 public class Constants {
 
     /** dynamo db indexes used for querying */
-    public static final String SNS_APPLICATION_ARN = "arn:aws:sns:eu-central-1:980738030415:app/GCM/LetMeGoAndroid"; // SNS application arn for push notifications
+    public static final String SNS_APPLICATION_ARN = "arn:aws:sns:eu-central-1:980738030415:app/GCM/LetMeGo"; // SNS application arn for push notifications
     public static final String CONTINENT_COUNTRY_INDEX = "continent-country-index";
     public static final String CONTINENT_GEOHASH_INDEX = "continent-geohash-index";
     public static final String CONTINENT_CRONDATE_INDEX ="continent-crondate-index";
@@ -16,5 +16,5 @@ public class Constants {
     public static final String USERNAME_ENDPOINT_INDEX ="username-endpoint-index";
 
     /** number of max devices per user which receive push notifications */
-    public static final int MAX_USER_DEVICES = 3;
+    public static final int MAX_USER_DEVICES = 100;
 }
